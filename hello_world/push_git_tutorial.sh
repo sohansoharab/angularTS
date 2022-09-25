a00e2727ee1cf6fa1b81d8e02b03ed58fb76b013
sohansoharab <ssohan911@gmail.com>
2022-09-25 11:49:06 +0600

    hello

--- hello_world/push_git_tutorial.sh
+++ hello_world/push_git_tutorial.sh
@@ -4,10 +4,15 @@ do
 	nowsec=$(date +%s)
 	delta=$((nowsec-modsec))
 
+	see="Something"
+
 	if [ $delta -lt 30 ]; then
-		git add .
-		git commit -m "hello"
-		git push
+		if [[ $see!="nothing to commit, working tree clean" ]]; then
+			git add .
+			git commit -m "hello"
+			see=`git commit -m "hello" | grep "nothing to commit"`
+			git push
+		fi
 	fi
 	modsec=$(date --utc --reference=tutorial.md +%s)
 	nowsec=$(date +%s)
